2f87eb35f6684c78341755980022ead2f0cf034b
manickam1024 <manicyam1234@gmail.com>
2025-10-12 14:05:15 +0530

    added tailwind css and installed daisy UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 function App() {
   return (
     <>
-      <h1>hello world</h1>
+      <div className="hover:bg-amber-100 font-bold text-3xl flex flex-col justify-center-safe items-center h-dvh ">
+        <p className="pb-7">hello world</p>
+        <button className="btn">Default</button>
+      </div>
     </>
   );
 }
